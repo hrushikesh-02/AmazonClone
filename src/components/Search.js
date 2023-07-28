3d5728d121d08df4086aaaf2a9e58ef4833a1c3b
Hrushikesh Patel <hrushikesh592002@gmail.com>
2023-07-28 18:53:47 +0530

    autocomplete functionality update

--- src/components/Search.js
+++ src/components/Search.js
@@ -19,6 +19,11 @@ const Search = () => {
   const [data, setData] = useState([
     { label: "The Shawshank Redemption", year: 1994 },
     { label: "The Godfather", year: 1972 },
+    { label: "The Godfather: Part II", year: 1974 },
+    { label: "The Dark Knight", year: 2008 },
+    { label: "12 Angry Men", year: 1957 },
+    { label: "Schindler's List", year: 1993 },
+    { label: "Pulp Fiction", year: 1994 },
   ]);
 
   const config = {
@@ -31,19 +36,21 @@ const Search = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("/data/products.json", config);
-        const jsonData = response.data;
-        setData(Object.values(jsonData));
-        console.log(data);
+        const response = await axios.get("/data/Product.json", config);
+        const jsonData = Object.values(response.data);
+        // const objectsArray = jsonData.map((item) => ({ title: item.title }));
+        const objectsArray = jsonData.map((item) => item.title);
+        setData(objectsArray);
       } catch (error) {
-        console.error("Error fetching asdata:", error);
+        console.error("Error fetching data:", error);
       }
     };
     fetchData();
   }, []);
+  console.log(data);
 
   return (
-    <div className="w-[100%] bg-white text-black flex rounded-[5px]">
+    <div className="w-[100%] bg-white text-black flex rounded-[5px] h-12">
       <div className="flex items-center rounded">
         <select className="bg-gray-300 text-black border text-xs xl:text-sm h-[100%] rounded-[5px_0px_0px_5px]">
           {catOpts.map((category) => {
@@ -56,18 +63,18 @@ const Search = () => {
         </select>
       </div>
       <div className="flex grow items-center justify-between">
-        <input
+        {/* <input
           className="flex grow items-center h-[100%] rounded-l text-black pl-2"
           type="text"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
-        />
-
+        /> */}
         <Autocomplete
-          disablePortal
+          type="text"
+          id="combo-box-demo"
           options={data}
-          sx={{ width: 300 }}
-          renderInput={(params) => <TextField {...params} label="Movie" />}
+          sx={{ border: "none", flexGrow: 1 }}
+          renderInput={(params) => <TextField {...params} />}
         />
       </div>
       <div className="bg-amazonclone-yellow flex items-center justify-center p-2  rounded-[0_5px_5px_0]">
